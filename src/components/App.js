de800353595313e76b36edc9416c7f68ab67a541
Alex Mojica <alexmojicamtz@gmail.com>
2021-03-12 13:00:25 +0100

    First Commit

---
+++ src/components/App.js
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="App">Hola desde App</div>;
+};
+
+export default App;
---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AppBar, Toolbar } from '@material-ui/core';
+
+const Header = (props) => {
+  return (
+    <AppBar>
+      <Toolbar>Material Ui Template</Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
